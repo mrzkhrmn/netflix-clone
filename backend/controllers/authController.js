954adc2298fb8ee477baca9f431e99c00956f680
mrzkhrmn <mrzkhrmn@hotmail..com>
2024-10-02 02:23:52 +0300

    Initializing vite setting up our express server then setting up auth routes and connecting to mongo database

---
+++ backend/controllers/authController.js
@@ -0,0 +1,23 @@
+export const signup = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import { connectDB } from "./db/connectDB.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use("/api/v1/auth", authRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port: ${process.env.PORT}`);
+});
+
+connectDB();
