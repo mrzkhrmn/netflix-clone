c8ae1eb3e615d04274216acfe2f97b50e541f3a2
mrzkhrmn <mrzkhrmn@hotmail..com>
2024-10-07 18:52:58 +0300

    adding protectRoute middleware

--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,19 @@ import movieRoutes from "./routes/movieRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import tvRoutes from "./routes/tvRoutes.js";
 import { connectDB } from "./db/connectDB.js";
+import cookieParser from "cookie-parser";
+import { protectRoute } from "./middleware/protectRoute.js";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/movie", movieRoutes);
-app.use("/api/v1/tv", tvRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port: ${process.env.PORT}`);
